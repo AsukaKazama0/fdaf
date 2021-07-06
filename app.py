09b1a086589847341a6cf7a2dbb10533f18c2005
AsukaKazama0 <80569912+AsukaKazama0@users.noreply.github.com>
2021-07-06 16:51:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask import request
 from runtime import getUri
+app.SECRET_KEY = b'_5#y2L"F4Q8z\n\xec]/'
 app = Flask(__name__)
-app.debug = True
+app.debug = False
 
 
 @app.route("/api/v4/", methods=["GET"])
@@ -12,13 +13,11 @@ def cop():
 
     if s != None:
         if s2 == None:
-            s2 = 'USDT'
+           s2 = 'USDT'
         returnUri = getUri(s,s2)
-        json = {"code": 200,"result":"sucess","imgUri":returnUri }
+        json = {"cod": 200,"result":"sucess","imgUri":returnUri }
     else:
         json = """{"code": 500,
         "result":"error"
         }"""
     return json
-
-app.run()
