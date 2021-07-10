5601d2f1f3cc8cc2f5737d3a25e5ea5171d3b52d
AsukaKazama0 <80569912+AsukaKazama0@users.noreply.github.com>
2021-07-10 16:43:50 +0530

    Update runtime.py

--- runtime.py
+++ runtime.py
@@ -14,14 +14,15 @@ import random
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
 
-chrome_options = webdriver.ChromeOptions()
-chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--disable-dev-shm-usage")
-chrome_options.add_argument("--no-sandbox")
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,options=chrome_options)
+
 
 def getUri(cop,cop2):
+	chrome_options = webdriver.ChromeOptions()
+	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+	chrome_options.add_argument("--headless")
+	chrome_options.add_argument("--disable-dev-shm-usage")
+	chrome_options.add_argument("--no-sandbox")
+	driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,options=chrome_options)
 	symbol = cop;
 	driver.get('https://s.tradingview.com/widgetembed/?frameElementId=tradingview_a6232&symbol={}{}&interval=D&symboledit=1&saveimage=1&toolbarbg=f1f3f6&studies=%5B%5D&theme=dark&style=1&timezone=Etc%2FUTC&studies_overrides=%7B%7D&overrides=%7B%7D&enabled_features=%5B%5D&disabled_features=%5B%5D&locale=in&utm_source=localhost&utm_medium=widget_new&utm_campaign=chart&utm_term=NASDAQ%3AAAPL'.format(symbol,cop2))
 	wait = WebDriverWait(driver, 20)
@@ -35,4 +36,3 @@ def getUri(cop,cop2):
 
 	driver.quit()
 	return y
-driver.quit()
