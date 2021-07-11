a9d99034368fcd4193fa97f311e24b6722edb9f0
AsukaKazama0 <80569912+AsukaKazama0@users.noreply.github.com>
2021-07-11 17:07:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,11 +10,19 @@ app.SECRET_KEY = b'_5#y2L"F4Q8z\n\xec]/'
 def cop():
     s =  request.args.get("s")
     s2 = request.args.get('sn')
-
+    timeFrame =reqquest.args.get('timeFrame')
+    Theme = request.args.get('Theme')
+    source = request.args.get('source')
+    if Theme == None:
+        theme = 'DARK'
+    if source == None:
+        source = 'Binance'
+    if timeFrame == None:
+        timeFrame = '4H'
     if s != None:
         if s2 == None:
            s2 = 'USDT'
-        returnUri = getUri(s,s2)
+        returnUri = getUri(s,s2,timeFrame,Theme,source)
         json = {"cod": 200,"result":"sucess","imgUri":returnUri }
     else:
         json = """{"code": 500,
