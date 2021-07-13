8cda7e33352b8da4aff1bb5dff68f67741445d79
AsukaKazama0 <80569912+AsukaKazama0@users.noreply.github.com>
2021-07-13 20:37:18 +0530

    Update runtime.py

--- runtime.py
+++ runtime.py
@@ -13,23 +13,27 @@ import random
 
 GOOGLE_CHROME_PATH = '/app/.apt/usr/bin/google_chrome'
 CHROMEDRIVER_PATH = '/app/.chromedriver/bin/chromedriver'
+# CHROMEDRIVER_PATH = './chromedriver'
+
 
 
 
 def getUri(cop,cop2,timeFrame,theme,source):
 	chrome_options = webdriver.ChromeOptions()
+	options.add_argument("--start-maximized")
 	chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 	chrome_options.add_argument("--headless")
 	chrome_options.add_argument("--disable-dev-shm-usage")
 	chrome_options.add_argument("--no-sandbox")
 	driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,options=chrome_options)
 	symbol = cop;
-	driver.get('https://in.tradingview.com/chart/?symbol={}%3{}{}&interval={}&theme={}'.format(source,symbol,cop2,timeFrame,theme))
+	driver.get('https://www.tradingview.com/widgetembed/?frameElementId=tradingview_9aa27&symbol={}{}&interval={}&theme={}'.format(symbol,cop2,timeFrame,theme))
 	wait = WebDriverWait(driver, 20)
 
 
 	elem = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.chart-gui-wrapper')))
 	clickable = driver.find_element_by_id('header-toolbar-screenshot').click();
+	time.sleep(4)
 	lem = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '.copyForm-1aOR8tBW')))
 	y = driver.find_element_by_class_name('input-3bEGcMc9').get_attribute("value")
 
